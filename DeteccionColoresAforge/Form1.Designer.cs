3d2288fd7b8fccbc0161f55c1b2442bcda305463
Yonathan Román <yonathanr11.12@gmail.com>
2019-04-03 18:42:14 -0600

    Git init

---
+++ DeteccionColoresAforge/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace DeteccionColoresAforge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbColor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(736, 464);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 496);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(263, 496);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(344, 498);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Marcar";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(754, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(324, 282);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbColor
+            // 
+            this.lbColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbColor.ForeColor = System.Drawing.Color.White;
+            this.lbColor.Location = new System.Drawing.Point(754, 297);
+            this.lbColor.Name = "lbColor";
+            this.lbColor.Size = new System.Drawing.Size(324, 23);
+            this.lbColor.TabIndex = 8;
+            this.lbColor.Text = "Color";
+            this.lbColor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 479);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Selecciona una camara:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1090, 526);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbColor);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbColor;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DeteccionColoresAforge/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using System.Drawing.Imaging;
+
+namespace DeteccionColoresAforge
+{
+    public partial class Form1 : Form
+    {
+
+        private FilterInfoCollection dispositivosDeVideo;
+        private VideoCaptureDevice fuentev = null;
+        private bool AforgeStatus = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (AforgeStatus)
+            {
+                fuentev.Stop();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dispositivosDeVideo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo dispositivo in dispositivosDeVideo)
+            {
+                comboBox1.Items.Add(dispositivo.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("Iniciar"))
+            {
+                fuentev = new VideoCaptureDevice(dispositivosDeVideo[comboBox1.SelectedIndex].MonikerString);
+                fuentev.NewFrame += new NewFrameEventHandler(fuentevNewFrame);
+                fuentev.Start();
+                AforgeStatus = true;
+                comboBox1.Enabled = false;
+                button1.Text = "Detener";
+            }
+            else
+            {
+                button1.Text = "Iniciar";
+                pictureBox1.Image = null;
+                pictureBox2.Image = null;
+                lbColor.Text = "Color";
+                lbColor.BackColor = Color.White;
+                lbColor.ForeColor = Color.Black;
+                AforgeStatus = false;
+                comboBox1.Enabled = true;
+                fuentev.Stop();
+            }
+            
+
+        }
+
+        string colorSelect = "amarillo";
+        int cont = 0;
+
+        void fuentevNewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap temp = video.Clone() as Bitmap;
+            
+            ColorFiltering filter = new ColorFiltering();
+            // set color ranges to keep
+
+            switch (colorSelect)
+            {
+                case "verde":
+                    filter.Red = new IntRange(0, 75);
+                    filter.Green = new IntRange(115, 255);
+                    filter.Blue = new IntRange(0, 120);
+                    break;
+                case "anaranjado":
+                    filter.Red = new IntRange(150, 255);
+                    filter.Green = new IntRange(75, 128);
+                    filter.Blue = new IntRange(0, 100);
+                    break;
+                case "azul":
+                    filter.Red = new IntRange(15, 80);
+                    filter.Green = new IntRange(50, 110);
+                    filter.Blue = new IntRange(129, 255);
+                    break;
+                case "rojo":
+                    filter.Red = new IntRange(115, 255);
+                    filter.Green = new IntRange(0, 80);
+                    filter.Blue = new IntRange(0, 75);
+                    break;
+                case "amarillo":
+                    filter.Red = new IntRange(140, 255);
+                    filter.Green = new IntRange(140, 255);
+                    filter.Blue = new IntRange(0, 100);
+                    break;
+                case "rosa":
+                    filter.Red = new IntRange(180, 255);
+                    filter.Green = new IntRange(57, 135);
+                    filter.Blue = new IntRange(112, 196);
+                    break;
+                    //default:
+                    //    colorSelect = "verde";
+                    //    break;
+
+            }
+            //filter.Radius = 90;
+            filter.FillOutsideRange = true;
+            filter.ApplyInPlace(temp);
+
+            //BlobCounter es una clase que se usa para distingir objetos, colores, etc;
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.MinWidth = 5;
+            blobCounter.MinHeight = 5;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+            blobCounter.ProcessImage(temp);
+
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+
+            //foreach (Rectangle rec in rects)
+            if (rects.Length > 0)
+            {
+                lbColor.Text = colorSelect;
+                lbColor.ForeColor = Color.White;
+                switch (colorSelect)
+                {
+                    case "verde":
+                        pictureBox2.Image = Properties.Resources.verde;
+                        break;
+                    case "anaranjado":
+                        pictureBox2.Image = Properties.Resources.anaranjado;
+                        break;
+                    case "azul":
+                        pictureBox2.Image = Properties.Resources.azul;
+                        break;
+                    case "rojo":
+                        pictureBox2.Image = Properties.Resources.rojo;
+                        break;
+                    case "amarillo":
+                        pictureBox2.Image = Properties.Resources.amarillo;
+                        break;
+                    case "rosa":
+                        pictureBox2.Image = Properties.Resources.rosa;
+                        break;
+                }
+                for (int i = 0; rects.Length > i; i++)
+                {
+                    
+
+                    Rectangle objectRect = rects[0];
+                    Graphics g = Graphics.FromImage(video);
+                    using (Pen pen = new Pen(Color.Red, 3))
+                    {
+
+                        if (checkBox1.Checked)
+                        {
+                            g.DrawRectangle(pen, objectRect);
+                            //g.DrawLine(pen, objectRect.Width- objectRect.Width, objectRect.Width- objectRect.Width, objectRect.Width, objectRect.Width);
+
+
+                            Bitmap ColorDetect = video;
+                            //Color x = ColorDetect.GetPixel(objectRect.Width/2, objectRect.Height/2);
+                            Color x = ColorDetect.GetPixel(objectRect.X + (objectRect.Width / 3), objectRect.Top + (objectRect.Height / 3));
+                            lbColor.BackColor = x;
+
+
+                            //PointF drawPoin = new PointF(objectRect.Width / 2, objectRect.Width / 2);
+                            //PointF drawPoin = new PointF(objectRect.X, objectRect.Y);
+                            PointF drawPoin = new PointF(objectRect.X + (objectRect.Width / 3), objectRect.Top + (objectRect.Height / 3));
+                            int objectX = objectRect.X + objectRect.Width / 2 - video.Width / 2;
+                            int objectY = video.Height / 2 - (objectRect.Y + objectRect.Height / 2);
+                            String Blobinformation = "X= " + objectX.ToString() + "\nY= " + objectY.ToString() + "\nSize=" + objectRect.Size.ToString();
+                            //g.DrawString(Blobinformation, new Font("Arial", 12), new SolidBrush(Color.Blue), drawPoin);
+                            g.DrawString("+", new Font("Arial", 12), new SolidBrush(Color.White), drawPoin);
+                            //g.DrawString((i+1).ToString(), new Font("Arial", 12), new SolidBrush(Color.White), objectRect);
+
+                        }
+                    }
+
+                    g.Dispose();
+                }
+            }
+            else
+            {
+                lbColor.Text = "Detectando...";
+                lbColor.BackColor = Color.White;
+                lbColor.ForeColor = Color.Black;
+                pictureBox2.Image = null;
+
+                switch (colorSelect)
+                {
+                    case "verde":
+                        colorSelect = "anaranjado";
+                        break;
+                    case "anaranjado":
+                        colorSelect = "azul";
+                        break;
+                    case "azul":
+                        colorSelect = "rojo";
+                        break;
+                    case "rojo":
+                        colorSelect = "amarillo";
+                        break;
+                    case "amarillo":
+                        colorSelect = "rosa";
+                        break;
+                    case "rosa":
+                        colorSelect = "verde";
+                        break;
+                }
+
+                cont++;
+                Console.WriteLine("ELSE " + cont + " - " + colorSelect);
+            }
+            
+            pictureBox1.Image = video;
+            //pictureBox2.Image = temp;
+        }
+
+        public Rectangle objectRect { get; set; }
+    }
+}
